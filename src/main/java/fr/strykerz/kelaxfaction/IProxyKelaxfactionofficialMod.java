c003d38cb4627396a61c463a2703d8ec5fd78fcf
strykerzOfficiel <unconfigured@null.spigotmc.org>
2020-11-14 01:12:06 +0100

    Set File

---
+++ src/main/java/fr/strykerz/kelaxfaction/IProxyKelaxfactionofficialMod.java
@@ -0,0 +1,16 @@
+package fr.strykerz.kelaxfaction;
+
+import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+public interface IProxyKelaxfactionofficialMod {
+	void preInit(FMLPreInitializationEvent event);
+
+	void init(FMLInitializationEvent event);
+
+	void postInit(FMLPostInitializationEvent event);
+
+	void serverLoad(FMLServerStartingEvent event);
+}
